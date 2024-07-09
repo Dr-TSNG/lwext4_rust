91693dadedba3f172a557c1dd85bb9c88c8e352b
Nullptr <noreply@nullptr.icu>
2024-07-09 11:04:25 +0800

    Add readlink

--- src/dir.rs
+++ src/dir.rs
@@ -29,6 +29,78 @@ impl Ext4Dir {
     pub fn inode(&self) -> u32 {
         self.0.f.inode
     }
+
+    pub fn mkdir(path: &str) -> Result<(), i32> {
+        let c_path = CString::new(path).unwrap();
+        match unsafe { ext4_dir_mk(c_path.as_ptr()) } {
+            0 => Ok(()),
+            e => {
+                error!("ext4_dir_mk {} failed: {}", path, e);
+                Err(e)
+            }
+        }
+    }
+
+    pub fn rmdir(path: &str) -> Result<(), i32> {
+        let c_path = CString::new(path).unwrap();
+        match unsafe { ext4_dir_rm(c_path.as_ptr()) } {
+            0 => Ok(()),
+            e => {
+                error!("ext4_dir_rm {} failed: {}", path, e);
+                Err(e)
+            }
+        }
+    }
+
+    pub fn rmfile(path: &str) -> Result<(), i32> {
+        let c_path = CString::new(path).unwrap();
+        match unsafe { ext4_fremove(c_path.as_ptr()) } {
+            0 => Ok(()),
+            e => {
+                error!("ext4_dir_rm {} failed: {}", path, e);
+                Err(e)
+            }
+        }
+    }
+
+    pub fn movedir(old_path: &str, new_path: &str) -> Result<(), i32> {
+        let c_old_path = CString::new(old_path).unwrap();
+        let c_new_path = CString::new(new_path).unwrap();
+        match unsafe { ext4_dir_mv(c_old_path.as_ptr(), c_new_path.as_ptr()) } {
+            0 => Ok(()),
+            e => {
+                error!("ext4_dir_mv {} to {} failed: {}", old_path, new_path, e);
+                Err(e)
+            }
+        }
+    }
+
+    pub fn movefile(old_path: &str, new_path: &str) -> Result<(), i32> {
+        let c_old_path = CString::new(old_path).unwrap();
+        let c_new_path = CString::new(new_path).unwrap();
+        match unsafe { ext4_frename(c_old_path.as_ptr(), c_new_path.as_ptr()) } {
+            0 => Ok(()),
+            e => {
+                error!("ext4_dir_mv {} to {} failed: {}", old_path, new_path, e);
+                Err(e)
+            }
+        }
+    }
+
+    pub fn readlink(path: &str) -> Result<String, i32> {
+        let c_path = CString::new(path).unwrap();
+        let mut buf = [0u8; 260];
+        let mut rcnt = 0;
+        unsafe {
+            match ext4_readlink(c_path.as_ptr(), buf.as_mut_ptr() as _, 260, &mut rcnt) {
+                0 => Ok(String::from_utf8_lossy(&buf[..rcnt as usize]).to_string()),
+                e => {
+                    error!("ext4_readlink {} failed: {}", path, e);
+                    Err(e)
+                }
+            }
+        }
+    }
 }
 
 impl Drop for Ext4Dir {
@@ -52,62 +124,5 @@ impl Iterator for Ext4Dir {
             name: String::from_utf8_lossy(name_buf).to_string(),
             type_: entry.inode_type,
         })
-    }
-}
-
-pub fn ext4_mkdir(path: &str) -> Result<(), i32> {
-    let c_path = CString::new(path).unwrap();
-    match unsafe { ext4_dir_mk(c_path.as_ptr()) } {
-        0 => Ok(()),
-        e => {
-            error!("ext4_dir_mk {} failed: {}", path, e);
-            Err(e)
-        }
-    }
-}
-
-pub fn ext4_rmdir(path: &str) -> Result<(), i32> {
-    let c_path = CString::new(path).unwrap();
-    match unsafe { ext4_dir_rm(c_path.as_ptr()) } {
-        0 => Ok(()),
-        e => {
-            error!("ext4_dir_rm {} failed: {}", path, e);
-            Err(e)
-        }
-    }
-}
-
-pub fn ext4_rmfile(path: &str) -> Result<(), i32> {
-    let c_path = CString::new(path).unwrap();
-    match unsafe { ext4_fremove(c_path.as_ptr()) } {
-        0 => Ok(()),
-        e => {
-            error!("ext4_dir_rm {} failed: {}", path, e);
-            Err(e)
-        }
-    }
-}
-
-pub fn ext4_movedir(old_path: &str, new_path: &str) -> Result<(), i32> {
-    let c_old_path = CString::new(old_path).unwrap();
-    let c_new_path = CString::new(new_path).unwrap();
-    match unsafe { ext4_dir_mv(c_old_path.as_ptr(), c_new_path.as_ptr()) } {
-        0 => Ok(()),
-        e => {
-            error!("ext4_dir_mv {} to {} failed: {}", old_path, new_path, e);
-            Err(e)
-        }
-    }
-}
-
-pub fn ext4_movefile(old_path: &str, new_path: &str) -> Result<(), i32> {
-    let c_old_path = CString::new(old_path).unwrap();
-    let c_new_path = CString::new(new_path).unwrap();
-    match unsafe { ext4_frename(c_old_path.as_ptr(), c_new_path.as_ptr()) } {
-        0 => Ok(()),
-        e => {
-            error!("ext4_dir_mv {} to {} failed: {}", old_path, new_path, e);
-            Err(e)
-        }
     }
 }
